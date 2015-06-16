883675d55d6da03b5d8de2d4aee98d1b336734e1
sukitha <sukithaj@gmail.com>
2015-06-16 15:14:52 +0530

    working version with db updates

--- Freeswitch/VoicePortal.lua
+++ Freeswitch/VoicePortal.lua
@@ -10,18 +10,38 @@ onbusy = argv[7];
 onanswer = argv[8];
 
 
-
+--DND, CALL_DIVERT, AVAILABLE
 
 session:set_tts_params("flite", "kal");
 session:speak("Welcome to the voice portal, Please enter your pin number");
 digits = session:getDigits(4, "#", 5000);
+
+	
+statusx = status
+	
+if status == 'DND' then
+
+statusx= "DND";
+	
+elseif status == 'AVAILABLE' then
+
+statusx = "ACTIVE"
+		
+elseif status == 'CALL_DIVERT' then
+
+
+statusx = "AWAY"
+			
+else
+			
+end
 
 
 
 if digits == pin then
 
 	session:flushDigits();
-	satatusString = string.format("You are successfully loged in to the syatem, your current state is %s, if you want to change status please press 1 or press 2 for status based actions",status);
+	satatusString = string.format("You are successfully loged in to the syatem, your current state is %s, if you want to change status please press 1 or press 2 for status based actions",statusx);
 	session:speak(satatusString);
 	digits = session:getDigits(1, "#", 5000);
 	
@@ -29,13 +49,17 @@ if digits == pin then
 	
 		session:speak("Pressed 1");
 		
-		if status == 'dnd' then
+		if status == 'DND' then
 		
 			session:flushDigits();
 			session:speak("You are in dnd mode, please press 1");
 			digits = session:getDigits(1, "#", 5000);
 			
 			if digits == "1" then 
+									
+			satatusUpdateQuery = string.format("UPDATE \"CSDB_PBXUsers\" SET \"UserStatus\" = 'AVAILABLE' WHERE \"UserUuid\" = '%s'", userid);
+			freeswitch.consoleLog("notice", satatusUpdateQuery);
+			dbh:query(satatusUpdateQuery);
 			
 			session:speak("You are now in active mode");
 			
@@ -46,7 +70,7 @@ if digits == pin then
 			end
 			
 		
-		elseif status == 'active' then
+		elseif status == 'AVAILABLE' then
 		
 			session:flushDigits();
 			session:speak("You are in active mode, please press 1 for go to away mode, press 2 for go to dnd mode");
@@ -56,9 +80,17 @@ if digits == pin then
 			
 			session:speak("You are now in away mode");
 			
+			satatusUpdateQuery = string.format("UPDATE \"CSDB_PBXUsers\" SET \"UserStatus\" = 'CALL_DIVERT' WHERE \"UserUuid\" = '%s'", userid);
+			freeswitch.consoleLog("notice", satatusUpdateQuery);
+			dbh:query(satatusUpdateQuery);
+			
 			elseif digits == "2" then 
 			
 			session:speak("Your are now in dnd mode");
+			
+			satatusUpdateQuery = string.format("UPDATE \"CSDB_PBXUsers\" SET \"UserStatus\" = 'DND' WHERE \"UserUuid\" = '%s'", userid);
+			freeswitch.consoleLog("notice", satatusUpdateQuery);
+			dbh:query(satatusUpdateQuery);
 			
 			else
 			
@@ -68,7 +100,7 @@ if digits == pin then
 		
 		
 		
-		elseif status == 'away' then
+		elseif status == 'CALL_DIVERT' then
 		
 			session:flushDigits();
 			session:speak("You are in away mode, If you want to go to active, please press 1");
@@ -77,6 +109,10 @@ if digits == pin then
 			if digits == "1" then 
 			
 			session:speak("You are now in active mode");
+			
+			satatusUpdateQuery = string.format("UPDATE \"CSDB_PBXUsers\" SET \"UserStatus\" = 'AVAILABLE' WHERE \"UserUuid\" = '%s'", userid);
+			freeswitch.consoleLog("notice", satatusUpdateQuery);
+			dbh:query(satatusUpdateQuery);
 			
 			else
 			
@@ -93,11 +129,11 @@ if digits == pin then
 	
 			session:speak("Pressed 2");
 		
-		if status == "dnd" then
+		if status == "DND" then
 			
 			session:speak("There are no action related to dnd status");
 		
-		elseif status == "away" then
+		elseif status == "CALL_DIVERT" then
 		
 			session:flushDigits();
 			session:speak("Please press 1 for divert call, press 2 for active voicemail");
@@ -109,15 +145,23 @@ if digits == pin then
 				session:flushDigits();
 				session:speak("Please enter divert number");
 				digits = session:getDigits(15, "#", 10000);
+				numdata = digits;
+				num = string.gsub (digits, "", " ");
 				
 				
 				session:flushDigits();
-				numberPressed = string.format("Number you entered is %s, please press 1 for confirm", digits);
+				numberPressed = string.format("Number you entered is %s, please press 1 for confirm", num);
 				session:speak(numberPressed);
 				digits = session:getDigits(1, "#", 5000);
 				
 				if digits == "1" then
 				
+					
+					
+					satatusUpdateQuery = string.format("UPDATE \"CSDB_PBXUserTemplates\" SET \"CallDivertNumber\" = '%s' WHERE \"id\" = %s",numdata, templateid);
+					freeswitch.consoleLog("notice", satatusUpdateQuery);
+					dbh:query(satatusUpdateQuery);
+					
 					session:speak("Your Divert number is set successfuly");
 				
 				else
@@ -128,6 +172,11 @@ if digits == pin then
 			
 			elseif digits == "2" then
 			
+			
+				satatusUpdateQuery = string.format("UPDATE \"CSDB_PBXUsers\" SET \"VoicemailEnabled\" = true WHERE \"UserUuid\" = '%s'", userid);
+				freeswitch.consoleLog("notice", satatusUpdateQuery);
+				dbh:query(satatusUpdateQuery);
+			
 				session:speak("Your voicemail activated");
 			
 			else
@@ -136,7 +185,7 @@ if digits == pin then
 		
 		
 		
-		elseif status == "active" then
+		elseif status == "AVAILABLE" then
 		
 			session:flushDigits();
 			session:speak("Please press 1 for set onbusy forwarding number, press 2 for set noanswer forwarding number");
@@ -147,13 +196,20 @@ if digits == pin then
 				session:flushDigits();
 				session:speak("Please enter onbusy forwarding number");
 				digits = session:getDigits(15, "#", 10000);
+				numdata = digits;
+				num = string.gsub (digits, "", " ");
 				
 				session:flushDigits();
-				numberPressed = string.format("Number you entered is %s, please press 1 for confirm", digits);
+				numberPressed = string.format("Number you entered is %s, please press 1 for confirm", num);
 				session:speak(numberPressed);
 				digits = session:getDigits(1, "#", 5000);
 				
 				if digits == "1" then
+				
+					satatusUpdateQuery = string.format("UPDATE \"CSDB_Forwardings\" SET \"DestinationNumber\" = '%s' WHERE \"id\" = %s",numdata, onbusy);
+					freeswitch.consoleLog("notice", satatusUpdateQuery);
+					dbh:query(satatusUpdateQuery);
+				
 				
 					session:speak("Your onbusy forwarding number set successfuly");
 				
@@ -170,13 +226,19 @@ if digits == pin then
 				session:flushDigits();
 				session:speak("Please enter noanswer forwarding number");
 				digits = session:getDigits(15, "#", 10000);
+				numdata = digits;
+				num = string.gsub (digits, "", " ");
 				
 				session:flushDigits();
-				numberPressed = string.format("Number you entered is %s, please press 1 for confirm", digits);
+				numberPressed = string.format("Number you entered is %s, please press 1 for confirm", num);
 				session:speak(numberPressed);
 				digits = session:getDigits(1, "#", 5000);
 				
 				if digits == "1" then
+				
+					satatusUpdateQuery = string.format("UPDATE \"CSDB_Forwardings\" SET \"DestinationNumber\" = '%s' WHERE \"id\" = %s",numdata, onanswer);
+					freeswitch.consoleLog("notice", satatusUpdateQuery);
+					dbh:query(satatusUpdateQuery);
 				
 					session:speak("Your noanswer forwarding number set successfuly");
 				
