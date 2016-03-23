e07718551517ae7ed8aab6e93d5ffdc514b24f2c
achinthau <achintha@duosoftware.com>
2016-03-23 18:42:37 +0530

    update mod_httapi and mod_flite

--- FreeswitchDocker/run.sh
+++ FreeswitchDocker/run.sh
@@ -12,9 +12,20 @@ sed -i 's^<!-- <load module="mod_xml_cdr"/> -->^<load module="mod_xml_cdr"/>^g' 
 sed -i 's^<!-- <load module="mod_xml_scgi"/> -->^<load module="mod_xml_scgi"/>^g' /usr/local/freeswitch/conf/autoload_configs/modules.conf.xml;
 sed -i 's^</modules>^<load module="mod_ards"/> </modules>^g' /usr/local/freeswitch/conf/autoload_configs/modules.conf.xml;
 sed -i 's^<!--<load module="mod_h26x"/>-->^<load module="mod_h26x"/>^g' /usr/local/freeswitch/conf/autoload_configs/modules.conf.xml;
+sed -i 's^<!-- <load module="mod_flite"/> -->^<load module="mod_flite"/>^g' /usr/local/freeswitch/conf/autoload_configs/modules.conf.xml;
 sed -i 's^<X-PRE-PROCESS cmd="set" data="global_codec_prefs=OPUS,G722,PCMU,PCMA,VP8"/>^<X-PRE-PROCESS cmd="set" data="global_codec_prefs=OPUS,G722,PCMU,PCMA,VP8,H.264,H.263,H264,H263-1998"/>^g' /usr/local/freeswitch/conf/vars.xml
 sed -i 's^<X-PRE-PROCESS cmd="set" data="outbound_codec_prefs=OPUS,G722,PCMU,PCMA,VP8"/>^<X-PRE-PROCESS cmd="set" data="outbound_codec_prefs=OPUS,G722,PCMU,PCMA,VP8,H.264,H.263,H264,H263-1998"/>^g' /usr/local/freeswitch/conf/vars.xml
 #echo '<load module="mod_ards"/>' >> /usr/local/freeswitch/conf/autoload_configs/modules.conf.xml;
+
+sed -i 's^<permission name="get-vars" value="false">^<permission name="get-vars" value="true">^g' /usr/local/freeswitch/conf/autoload_configs/httapi.conf.xml;
+sed -i 's^<application name="hangup"/>^<application name="hangup"/> <application name="play_and_get_digits"/> <application name="execute"/>^g' /usr/local/freeswitch/conf/autoload_configs/httapi.conf.xml;
+sed -i 's^<permission name="dial-set-context" value="false"/>^<permission name="dial-set-context" value="true"/>^g' /usr/local/freeswitch/conf/autoload_configs/httapi.conf.xml;
+sed -i 's^<permission name="dial-set-dialplan" value="false"/>^<permission name="dial-set-dialplan" value="true"/>^g' /usr/local/freeswitch/conf/autoload_configs/httapi.conf.xml;
+sed -i 's^<permission name="dial-set-cid-name" value="false"/>^<permission name="dial-set-cid-name" value="true"/>^g' /usr/local/freeswitch/conf/autoload_configs/httapi.conf.xml;
+sed -i 's^<permission name="dial-set-cid-number" value="false"/>^<permission name="dial-set-cid-number" value="true"/>^g' /usr/local/freeswitch/conf/autoload_configs/httapi.conf.xml;
+sed -i 's^<permission name="dial-full-originate" value="false"/>^<permission name="dial-full-originate" value="true"/>^g' /usr/local/freeswitch/conf/autoload_configs/httapi.conf.xml;
+sed -i 's^<permission name="conference-set-profile" value="false"/>^<permission name="conference-set-profile" value="true"/>^g' /usr/local/freeswitch/conf/autoload_configs/httapi.conf.xml;
+
 
 #sed -i 's^param name="listen-ip" value="::"^param name="listen-ip" value="0.0.0.0"^g' /usr/local/freeswitch/conf/autoload_configs/event_socket.conf.xml;
 if [[ ! -z $SYS_FS_EVENTSOCKET_PORT ]]
