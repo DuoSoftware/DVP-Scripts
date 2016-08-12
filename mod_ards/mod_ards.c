2d8a8edf612cdc8a6d9b406eb940115843e97f56
sukitha chanaka jayasingha <sukithaj@gmail.com>
2016-08-12 13:00:56 +0530

    Update mod_ards.c

--- mod_ards/mod_ards.c
+++ mod_ards/mod_ards.c
@@ -25,7 +25,7 @@
 *
 * Marc Olivier Chouinard <mochouinard@moctel.com>
 * Emmanuel Schmidbauer <e.schmidbauer@gmail.com>
-* Õtalo Rossi <italorossib@gmail.com>
+* √çtalo Rossi <italorossib@gmail.com>
 *
 * mod_ards.c -- Call Center Module
 *
@@ -1385,7 +1385,7 @@ static void *SWITCH_THREAD_FUNC outbound_agent_thread_run(switch_thread_t *threa
 
 					if (globals.uurl){
 						char uploaddata[1000];
-						switch_snprintf(uploaddata, sizeof(uploaddata), "curl_sendfile:%s/%s/%s file=%s class=CALLSERVER&type=CALL&category=CONVERSATION&referenceid=%s&display=%s-%s", globals.uurl, h->tenant, h->company, switch_channel_get_variable(member_channel, "ards_record_file"), h->member_uuid, caller_number, h->originate_user);
+						switch_snprintf(uploaddata, sizeof(uploaddata), "curl_sendfile:%s/%s/%s file=%s class=CALLSERVER&type=CALL&category=CONVERSATION&mediatype=audio&filetype=wav&referenceid=%s&display=%s-%s", globals.uurl, h->tenant, h->company, switch_channel_get_variable(member_channel, "ards_record_file"), h->member_uuid, caller_number, h->originate_user);
 						expandedx = switch_channel_expand_variables(member_channel, uploaddata);
 						switch_channel_set_variable(member_channel, "record_post_process_exec_api", expandedx);
 
