e2af1082eadef7dac293eb6c77b7e5d48c142b4e
achinthau <achintha@duosoftware.com>
2016-09-06 09:56:09 +0530

    update mongodbbackup.sh to create tar file

--- backup_scripts/mongodbbackup.sh
+++ backup_scripts/mongodbbackup.sh
@@ -3,7 +3,10 @@ DIR=`date +%m%d%y`
 DEST=/home/backups/database/mongo/$DIR 
 mkdir $DEST 
 mongodump -d admin -u duo -p DuoS123 -o $DEST 
-sshpass -p 'DuoS123' rsync -aruv /home/backups/database/mongo/$DIR root@192.168.51.251:/home/backups/database/mongodb
+cd $DEST;
+cd ../;
+tar -czf $DIR.tar.gz $DIR/
+sshpass -p 'DuoS123' rsync -aruv /home/backups/database/mongo/$DIR.tar.gz root@192.168.51.251:/home/backups/database/mongodb
 find /db_backups/* -type d -ctime +30  -exec rm -rf '{}' ';'
 #find /db_backups/* -type -d -ctime +30 -exec rm ­rf {} \
 
