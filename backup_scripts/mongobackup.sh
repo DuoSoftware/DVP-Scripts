1ecee71c50334fa59a0ef4192c5c229052826489
achinthau <achintha@duosoftware.com>
2017-10-31 14:30:19 +0530

    update backup_scripts/backup_voice_record.sh mongobackup.sh

--- backup_scripts/mongobackup.sh
+++ backup_scripts/mongobackup.sh
@@ -9,7 +9,7 @@ PASSWORD=DuoS123
 FILE=`date +"%Y%m%d%H%M"`
 OUTPUT_FILE=${BACKUP_DIR}/${FILE}
 mongodump -h $MONGO_HOST -d $DATABASE -u $USER -p $PASSWORD -o $OUTPUT_FILE
-# gzip the mysql database dump file
+## gzip the mysql database dump file
 tar -zcvf $OUTPUT_FILE.tar.gz -C $OUTPUT_FILE .
 rm -rf $OUTPUT_FILE
 # prune old backups
