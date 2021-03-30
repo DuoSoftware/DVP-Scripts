5d357158c2e565739816f5ef0195a2da0d8f1ece
ThuDu <itsthudu@gmail.com>
2021-03-30 09:34:44 +0530

    veery script optimize
    added nginx.sh
    commented unwated services from the script
    added DASHBOARD_REDIS_PASSWORD

--- veery_cloud_container/veery.sh
+++ veery_cloud_container/veery.sh
@@ -24,23 +24,6 @@ IFS=', ' read -r -a array <<< "$DEPLOY";
 #curl -sSL https://get.docker.com/ | sh
 #curl -sSL https://get.docker.com/gpg | sudo apt-key add -
 
-# Install Nginx-Proxy
-
-cd /usr/src/;
-if [ $REPOSITORY = "local" ]; then
- docker pull $REPOSITORY_IPURL":5000"/"nginx-proxy:latest";
- docker tag $REPOSITORY_IPURL":5000"/"nginx-proxy:latest" "nginx-proxy:latest";
- docker rmi -f $REPOSITORY_IPURL":5000"/"nginx-proxy:latest";
-elif [ $REPOSITORY = "github" ]; then
-if [ ! -d "DVP-ReverseProxy" ]; then
-	git clone https://github.com/DuoSoftware/DVP-ReverseProxy.git;
-fi
-cd DVP-ReverseProxy;
-docker build -t "nginx-proxy:latest" .
-fi
-cd /usr/src/;
-#docker run -d -p 80:80 -v /var/run/docker.sock:/tmp/docker.sock:ro -v /etc/localtime:/etc/localtime:ro --log-opt max-size=10m --restart=always --log-opt max-file=10 --name nginx nginx-proxy
-
 # install services
 
 for index in "${!array[@]}"
